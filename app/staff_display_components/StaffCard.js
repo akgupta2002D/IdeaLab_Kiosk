48f185ad22f00842aba8a795b5d797503d130a45
akgupta2002D <akgupta2002d@gmail.com>
2025-02-07 21:22:38 -0500

    Add Staff card component

---
+++ app/staff_display_components/StaffCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './StaffCard.module.css';
+
+const StaffCard = ({ staff }) => {
+  const { id, name, picture, classYear } = staff;
+  return (
+    <div key={id} className={styles.card}>
+      <img src={picture} alt={name} className={styles.image} />
+      <div className={styles.details}>
+        <h3 className={styles.name}>{name}</h3>
+        <p className={styles.classYear}>Class of {classYear}</p>
+      </div>
+    </div>
+  );
+};
+
+export default StaffCard;
