94191e7fb3f7c2d0dc200302ae1bef71f9880b72
akgupta2002D <akgupta2002d@gmail.com>
2025-02-28 08:51:40 -0500

    Fix Event Display popup content for clarity

--- app/components/homepage/EventInfo.js
+++ app/components/homepage/EventInfo.js
@@ -5,30 +5,34 @@ import styles from "./styles/EventInfo.module.css";
 const EventInfo = ({ name, description, date, location }) => {
   return (
     <div className={styles.eventOverlay}>
-      <h2 className={styles.eventTitle}>{name}</h2>
-      <p className={styles.eventDescription}>
-        {description || "No description! But it will be fun!"}
-      </p>
+      <div className={styles.eventTitleAndDesDiv}>
+        <h2 className={styles.eventTitle}>{name}</h2>
+        <p className={styles.eventDescription}>
+          {description || "No description! But it will be fun!"}
+        </p>
+      </div>
 
       <div className={styles.eventMeta}>
         <div>
-          <h6>Date</h6>
-          <p>{date}</p>
+          <h6>Date: {date}</h6>
+          
         </div>
         <div>
-          <h6>Location</h6>
-          <p>{location}</p>
+          <h6>Location: {location}</h6>
+          
         </div>
-      </div>
-
-      {/* Location image, if you want it in the overlay */}
-      <div className={styles.locationImageWrapper}>
+        {/* Location image, if you want it in the overlay */}
+        <div className={styles.locationImageWrapper}>
         <img
           src="/CIS_Location.jpeg"
           alt="Event Location"
           className={styles.locationImage}
         />
       </div>
+      </div>
+
+      
+      
     </div>
   );
 };
