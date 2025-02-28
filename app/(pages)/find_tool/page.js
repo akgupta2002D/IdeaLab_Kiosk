1480f03194886beb1ed2107a15c6a566ed68c0e5
akgupta2002D <akgupta2002d@gmail.com>
2025-02-28 08:05:32 -0500

    Add Placeholder info on the new kiosk button pages

---
+++ app/(pages)/find_tool/page.js
@@ -0,0 +1,7 @@
+export default function FindToolPage() {
+    return (
+      <div>
+        <h1>This is Find Tool page</h1>
+      </div>
+    );
+  }
---
+++ app/(pages)/ideabot/page.js
@@ -0,0 +1,7 @@
+export default function IdeabotPage() {
+    return (
+      <div>
+        <h1>This is Ideabot page</h1>
+      </div>
+    );
+  }
