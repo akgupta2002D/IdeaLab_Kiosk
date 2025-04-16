eb7b0c0f2a1de30fadb4590ff675efb00fd75612
akgupta2002D <akgupta2002d@gmail.com>
2025-04-16 16:24:20 -0400

    Add Skidmore Green to the stats

--- app/(pages)/admin/dashboard/page.js
+++ app/(pages)/admin/dashboard/page.js
@@ -6,8 +6,8 @@ export default function DashboardPage() {
 
     const pageContent = [
         {
-          name: 'Dashboard',
-          description: 'This is an overview page for the usage and changes of Idealab Kiosk.'
+          name: '',
+          description: ''
         },
     ]
 
